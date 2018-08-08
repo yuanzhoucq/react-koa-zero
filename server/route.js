c194e539778a9a82ac93e120c213554d9cfd6918
yuanzhoucq <yuanzhou.cq@gmail.com>
2018-08-08 16:47:36 +0200

    admin log info init

--- server/route.js
+++ server/route.js
@@ -1,6 +1,6 @@
 const Router = require('koa-router');
-const {sayHelloWorld} = require("./controller/HelloController");
-const {login, getUser} = require("./controller/AuthController");
+const {sayHelloWorld} = require("./controllers/HelloController");
+const {login, getUser} = require("./controllers/AuthController");
 
 const router = new Router();
 
