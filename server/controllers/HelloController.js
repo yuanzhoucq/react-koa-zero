5375bb9388080e780fc08300f9297e20a7097af0
zd <zidong@europass.paris>
2018-08-14 15:33:12 +0200

    base controller

--- server/controllers/HelloController.js
+++ server/controllers/HelloController.js
@@ -1,12 +1,12 @@
-//const HelloModel = require("../models/HelloModel");
+const HelloModel = require("../models/HelloModel");
 
 
 const BaseController = require('./BaseController');
 
 class HelloController extends BaseController {
 	async sayHelloWorld(ctx) {
- 		//ctx.body = `Hello, ${await HelloModel.getHelloName()}!`
- 		ctx.body = "Hello";
+ 		ctx.body = `Hello, ${await HelloModel.getHelloName()}!`
+ 		//ctx.body = "Hello";
 	}
 
 }
